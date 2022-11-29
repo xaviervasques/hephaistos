6a3c8b2d1b0fa22ef4b191335d1af64cd7d8c596
Xavier Vasques <vasquesxavier@hotmail.com>
2022-11-29 09:19:46 +0100

    Add files via upload

---
+++ requirements.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# ml_pipeline_function.py
+# Author: Xavier Vasques (Last update: 29/05/2022)
+
+# Copyright 2022, Xavier Vasques. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+# All required dependencies
+
+numpy
+pandas
+scipy
+sklearn
+category_encoders
+tensorflow
+matplotlib
+qiskit
+qiskit_machine_learning
+hashlib
+
+
+
